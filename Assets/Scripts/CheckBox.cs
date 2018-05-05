70753d8518a9ef964faf10cd44f5905bd59850a9
Taylor Anderson <taylor-anderson@hotmail.ca>
2018-05-05 16:06:16 -0400

    bug checkbox, camera, other stuff

---
+++ Assets/Scripts/CheckBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoodleStudio95;
+public class CheckBox : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject checkbox;
+	private DoodleAnimator checkboxAnim;
+	void Start () {	
+		this.checkboxAnim = checkbox.GetComponent<DoodleAnimator>();
+		print(checkboxAnim.Keyframes);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

95e91b6cf045215785334544be99ed943e0c5866
Taylor Anderson <taylor-anderson@hotmail.ca>
2018-05-05 18:46:47 -0400

    i dont even know

--- Assets/Scripts/CameraFrame.cs
+++ Assets/Scripts/CameraFrame.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using DoodleStudio95;
 public class CameraFrame : MonoBehaviour {
 
+	[Range(0, 1)]
+	public float zoom = 0;
+
+	public DoodleAnimator zoomBarAnim;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		zoomBarAnim.GoToAndPause(Mathf.RoundToInt(map(this.zoom, 0, 1, 0, 7)));
 		
+	}
+	float map(float x, float fromMin, float fromMax, float toMin, float toMax){
+		return toMin + ((x - fromMin) / (fromMax - fromMin)) * (toMax - toMin);
 	}
 }

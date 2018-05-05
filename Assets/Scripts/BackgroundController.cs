752f6d5f49797db7fb905665d8c2113042d6b5f9
Taylor Anderson <taylor-anderson@hotmail.ca>
2018-05-05 13:21:24 -0400

    title screen

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BackgroundController : MonoBehaviour {
+	private RawImage bg;
+	// Use this for initialization
+	void Start () {
+		bg = GetComponent<RawImage>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rect uvRect = bg.uvRect;
+		uvRect.x -= 0.002f;
+		uvRect.y -= 0.002f;
+		bg.uvRect = uvRect;
+	}
+}

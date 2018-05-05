5382d78a0d3b2ecba2e6a9f8d1938e75f9f121a0
Daniel Orellana <daniel.orellana@vretta.com>
2018-05-05 15:12:23 -0400

    camera work

---
+++ Assets/Scripts/Camera/SnapshotCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapshotCamera : CameraState {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	override public void StateEnter(CameraManager manager)
+    {
+
+	}
+	
+	override public void StateUpdate(CameraManager manager)
+    {
+		manager.MouseCameraX();
+		manager.FirstPersonMouseCameraY();
+	}
+
+	override public void StateExit(CameraManager manager)
+    {
+
+	}
+}
---
+++ Assets/Scripts/Camera/WanderCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderCamera : CameraState {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	override public void StateEnter(CameraManager manager)
+    {
+
+	}
+	
+	override public void StateUpdate(CameraManager manager)
+    {
+
+	}
+
+	override public void StateExit(CameraManager manager)
+    {
+
+	}
+
+}

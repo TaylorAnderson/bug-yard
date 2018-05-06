42988e77034d4d8e331e9d14d3959d95991ded0f
Taylor Anderson <taylor-anderson@hotmail.ca>
2018-05-06 14:13:03 -0400

    added camera trigger, made some other adjustments that i instantly forgot

---
+++ Assets/Scripts/Camera/CameraTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+
+	public List<BugType> bugsInView = new List<BugType>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(bugsInView.Count);
+	}
+	public void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Bug")) {
+			var bug = other.GetComponent<Bug>();
+			bugsInView.Add(bug.bugType);
+		}
+	}
+	public void OnTriggerExit(Collider other) {
+		if (other.CompareTag("Bug")) {
+			var bug = other.GetComponent<Bug>();
+			bugsInView.Remove(bug.bugType);
+		}
+	}
+}

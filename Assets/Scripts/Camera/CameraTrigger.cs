d1b71161d2dc0cbaf45f9d6d28b451d777666f92
Sandra Koo <mistralsa@gmail.com>
2018-05-06 17:05:24 -0400

    Camera checking before removing

--- Assets/Scripts/Camera/CameraTrigger.cs
+++ Assets/Scripts/Camera/CameraTrigger.cs
@@ -38,9 +38,12 @@ public class CameraTrigger : MonoBehaviour {
 		if (other.CompareTag("Bug")) {
 			var bug = other.GetComponent<Bug>();
 			bugsInView.Remove(bug.bugType);
-			GameObject particle = particleSystems[bug];
+			GameObject particle = null;
+			particleSystems.TryGetValue(bug, out particle);
 			particleSystems.Remove(bug);
-			Destroy(particle);
+			if (particle != null) {
+				Destroy(particle);
+			}
 		}
 	}
 	

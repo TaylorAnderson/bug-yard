0bc9790d240a50f58a8e916d48871f29960e1d1b
Sandra Koo <mistralsa@gmail.com>
2018-05-06 14:54:48 -0400

    Player walk anim!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DoodleStudio95;
 
 public class Player : MonoBehaviour {
 
@@ -9,16 +10,38 @@ public class Player : MonoBehaviour {
 	private float _cameraLerpSpeed = 0.2f;
 	public Vector3 positionWander;
 	public Vector3 positionCamera;
+	
+	//Animation stuff
+	private Vector3 lastPosition;
+	public DoodleAnimationFile standAnim;
+	public DoodleAnimationFile walkAnim;
+	private DoodleAnimator doodleGirl;
+	private DoodleAnimationFile currentAnim;
 
 	// Use this for initialization
 	void Start () {
 		_camera = GetComponentInChildren<Camera>();
 		m_camera_manager = GetComponentInChildren<CameraManager>();
+		lastPosition = transform.position;
+		doodleGirl = GetComponentInChildren<DoodleAnimator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if ((lastPosition - transform.position).magnitude > 0.05) {
+			if (currentAnim != walkAnim) {
+				currentAnim = walkAnim;
+				doodleGirl.ChangeAnimation(walkAnim);
+			}
+		}
+		else {
+			if (currentAnim != standAnim) {
+				currentAnim = standAnim;
+				doodleGirl.ChangeAnimation(standAnim);
+			}
+		}
+
+		lastPosition = transform.position;
 	}
 
 	public void GoToCameraView()
